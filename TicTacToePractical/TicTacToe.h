59f84230f9b45eb423d2891559623e3c68a24e99
Ben Sapo <bensapo@gmail.com>
2016-03-08 15:09:26 +0200

    Added print function

--- TicTacToePractical/TicTacToe.h
+++ TicTacToePractical/TicTacToe.h
@@ -10,20 +10,30 @@ private:
 	string Board[3][3];
 public:
 	TicTacToe();
-	bool move(short row, short col){
-		if (Board[row][col].empty()){
-			if (row <= 3 && row >= 0 && col <= 3 && col >= 0){
+	bool move(int row, int col){
+		if (Board[row-1][col-1].empty()){
+			if (row <= 2 && row >= 0 && col <= 2 && col >= 0){
 				return true;
 			}
 		}
 		return false;
+	}
+	void print(){
+		int i, j;
+		for (i = 0; i < 3; i++){
+			for (j = 0; j < 3; j++){
+				if (Board[i][j].empty())
+					cout << "-  ";
+				else
+				cout << Board[i][j] << "  ";
+			}
+			cout << endl;
+		}
 	}
 	~TicTacToe();
 };
 
 int main(){
 	TicTacToe game;
-	bool flag;
-	flag=game.move(0,0);
-	cout << flag;
+	game.print();
 }
