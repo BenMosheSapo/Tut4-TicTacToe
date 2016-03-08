64629d21a9992d5675ff448f51bfcbcf448ab465
Ben Sapo <bensapo@gmail.com>
2016-03-08 15:22:32 +0200

    Reset function added

--- TicTacToePractical/TicTacToe.cpp
+++ TicTacToePractical/TicTacToe.cpp
@@ -1,11 +1,55 @@
 #include "TicTacToe.h"
-
+#include <string>
+#include <string.h>
+#include <iostream>
 
 TicTacToe::TicTacToe()
 {
+	Board[0][2] = "x";
 }
 
-
+bool TicTacToe:: move(int row, int col){
+	row -= 1;
+	col -= 1;
+	if (Board[row][col].empty()){
+		if (row <= 2 && row >= 0 && col <= 2 && col >= 0){
+			return true;
+		}
+	}
+	return false;
+}
+void TicTacToe:: print(){
+	int i, j;
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++){
+			if (Board[i][j].empty())
+				cout << "-  ";
+			else
+				cout << Board[i][j] << "  ";
+		}
+		cout << endl;
+	}
+}
+void TicTacToe::reset(){
+	int i, j;
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++){
+			Board[i][j] = "";
+		}
+	}
+}
 TicTacToe::~TicTacToe()
 {
+}
+
+int main(){
+	TicTacToe game;
+	bool flag;
+	flag = game.move(1, 3);
+	cout << flag << endl;
+	game.print();
+	game.reset();
+	flag = game.move(1, 3);
+	cout << flag << endl;
+	game.print();
 }
--- TicTacToePractical/TicTacToe.h
+++ TicTacToePractical/TicTacToe.h
@@ -1,8 +1,5 @@
 #pragma once
-#pragma once
 #include <string>
-#include <string.h>
-#include <iostream>
 using namespace std;
 class TicTacToe
 {
@@ -10,30 +7,9 @@ private:
 	string Board[3][3];
 public:
 	TicTacToe();
-	bool move(int row, int col){
-		if (Board[row-1][col-1].empty()){
-			if (row <= 2 && row >= 0 && col <= 2 && col >= 0){
-				return true;
-			}
-		}
-		return false;
-	}
-	void print(){
-		int i, j;
-		for (i = 0; i < 3; i++){
-			for (j = 0; j < 3; j++){
-				if (Board[i][j].empty())
-					cout << "-  ";
-				else
-				cout << Board[i][j] << "  ";
-			}
-			cout << endl;
-		}
-	}
+	bool move(int row, int col);
+	void print();
+	void reset();
 	~TicTacToe();
 };
 
-int main(){
-	TicTacToe game;
-	game.print();
-}
